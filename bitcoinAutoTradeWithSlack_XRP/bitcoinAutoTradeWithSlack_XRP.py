4b583f32d09908bff9261045b2e6e301f65ffa3f
leeyeol0310 <78648032+leeyeol0310@users.noreply.github.com>
2021-04-30 10:14:45 +0900

    Update bitcoinAutoTradeWithSlack_XRP.py

--- bitcoinAutoTradeWithSlack_XRP/bitcoinAutoTradeWithSlack_XRP.py
+++ bitcoinAutoTradeWithSlack_XRP/bitcoinAutoTradeWithSlack_XRP.py
@@ -54,7 +54,7 @@ def get_current_price(ticker):
 upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
 # 시작 메세지 슬랙 전송
-post_message(myToken,"#stock", "autotrade start")
+post_message(myToken,"#coin", "autotrade start")
 
 # 자동매매 시작
 while True:
@@ -65,7 +65,7 @@ while True:
 
         # 09:00 < 현재 < 08:59:50 현재 시간이 이조건을 만족하면
         if start_time < now < end_time - datetime.timedelta(seconds=10): # 시작시간괴 끝나는시간
-            target_price = get_target_price("KRW-XRP", 0.5) # get_target_price(변동성전략 매수가)를 내가 원하는데로 구현해서 0.5 = K값을 조정해서 할수도있다
+            target_price = get_target_price("KRW-XRP", 0.1) # get_target_price(변동성전략 매수가)를 내가 원하는데로 구현해서 0.5 = K값을 조정해서 할수도있다
             """변동성 돌파 전략으로 매수 목표가 조회"""
 
             ma5 = get_ma5("KRW-XRP") # 현재 가격이
@@ -75,18 +75,18 @@ while True:
                 # current_price : 현재가격이 높다면
 
                 krw = get_balance("KRW") # 그떄 KRW 내 원화 잔고를 조회하고
-                if krw > 1000: # 이게 최소 거래 금액이 5천원 이상이면
+                if krw > 5000: # 이게 최소 거래 금액이 5천원 이상이면
                     buy_result = upbit.buy_market_order("KRW-XRP", krw*0.9995) # 코인을 매수
-                    post_message(myToken,"#stock", "XRP buy : " +str(buy_result))
+                    post_message(myToken,"#coin", "XRP buy : " +str(buy_result))
         else: # 09:00 < 현재 < 08:59:50 -> 8시59분50초 에서 9시 전일때
             XRP = get_balance("XRP") # 당일 종가에 코인을 전량 매도 하는 코드
             if XRP > 0.00008: # 현재 가지고있는 금액이 5천원 이상이면 전량 매도 하는 코드
                 sell_result = upbit.sell_market_order("KRW-XRP", XRP*0.9995)
-                post_message(myToken,"#stock", "XRP buy : " +str(sell_result))
+                post_message(myToken,"#coin", "XRP buy : " +str(sell_result))
         time.sleep(1)
     except Exception as e:
         print(e)
-        post_message(myToken,"#stock", e)
+        post_message(myToken,"#coin", e)
         time.sleep(1)
 
   
